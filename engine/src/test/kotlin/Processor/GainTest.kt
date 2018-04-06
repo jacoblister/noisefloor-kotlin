a5bac4bb9970150aec03918a29085c2cc428e589
Jacob Lister <jacob.lister@nzregen.co.nz>
2018-04-06 18:49:05 +1200

    Gradle build copy js modules

--- engine/src/test/kotlin/Processor/GainTest.kt
+++ engine/src/test/kotlin/Processor/GainTest.kt
@@ -1,14 +1,14 @@
-//package org.noisefloor.engine
-//
-//import kotlin.test.*
-//
-//class GainTest {
-//    @Test fun defaultMasterValue() {
-//        val gain = Gain()
-//        assertEquals(1f, gain.master.value)
-//    }
-//
-//    @Ignore fun baz() {
-//        fail("Doesn't work")
-//    }
-//}
+package org.noisefloor.engine
+
+import kotlin.test.*
+
+class GainTest {
+    @Test fun defaultMasterValue() {
+        val gain = Gain()
+        assertEquals(1f, gain.master.value)
+    }
+
+    @Ignore fun baz() {
+        fail("Doesn't work")
+    }
+}
