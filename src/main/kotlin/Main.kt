a44e6be3b6e901c83c25940a6c839e42e7543dd8
Jacob Lister <Jacob.Lister@liverton.com>
2018-04-09 01:12:52 +1200

    Add browser MIDI keyboard

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 package org.noisefloor.web
-
 import org.noisefloor.engine.Gain
+
+import org.w3c.dom.svg.*
+import kotlin.browser.document
 
 fun main(args : Array<String>) {
     val gain = Gain()
     println("Noisefloor web gain=${gain.master.value}")
+
+    val svg = document.getElementById("keyboard") as SVGSVGElement
+    val keyboard = Keyboard()
+    svg.appendChild(keyboard.render())
 }
 
 fun webstart() {
