56aa3374da884e5ed9d9003981a6ab3c9f3dfb5d
Jacob Lister <jacob.lister@nzregen.co.nz>
2018-06-16 13:33:32 +1200

    C++ Note allocator

--- native/Processor/MIDIInput.hpp
+++ native/Processor/MIDIInput.hpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 
 class MIDIInput {
-public:
+ public:
     bool polyPhonic;
     int channels = 4;
 
@@ -14,7 +14,7 @@ public:
     std::unordered_map<int, int> noteChannels;
     int nextChannel;
     int triggerClear;
-public:
+ public:
     inline void start(int sampleRate) {
         this->channelNotes.resize(this->channels, 0);
         this->channelData.resize(this->channels, {0,0,0});
@@ -27,6 +27,45 @@ public:
             struct MIDIEvent& midiEvent = midiInput.at(i);
             int note     = midiEvent.data[1];
             int velocity = midiEvent.data[2];
+
+            // note release or new not - free allocated channel
+            if (this->noteChannels.count(note) != 0) {
+                int noteChannel = this->noteChannels.at(note);
+                this->channelNotes[noteChannel] = 0;
+                this->channelData[noteChannel][1] = 0;
+                this->channelData[noteChannel][2] = 0;
+                this->noteChannels.erase(note);
+            }
+
+            if (velocity > 0) {
+                // Calculate frequency and level for note
+                float frequency = 220.0 * pow(2.0, (float)(note - 57) / 12);
+                float level = (float)velocity / 127;
+
+                // Allocate next free channel
+                int targetChannel = this->nextChannel;
+                while (this->channelNotes[targetChannel] != 0) {
+                    targetChannel++;
+                    if (targetChannel >= this->channels) { targetChannel = 0; }
+
+                    // If all channels active use current target
+                    if (targetChannel == this->nextChannel) {
+                        this->channelNotes[targetChannel] = 0;
+                        noteChannels.erase(this->channelNotes[targetChannel]);
+                    }
+                }
+
+                // set next channel, round robin
+                this->nextChannel = targetChannel + 1;
+                if (this->nextChannel >= this->channels) { this->nextChannel = 0; }
+
+                // set channel active
+                this->channelNotes[targetChannel] = note;
+                this->channelData[targetChannel][0] = frequency;
+                this->channelData[targetChannel][1] = level;
+                this->channelData[targetChannel][2] = level;
+                this->noteChannels[note] = targetChannel;
+            }
         }
 
         this->triggerClear = 2;
